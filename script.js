39158b9b64cca721af168586e9cd32a68a92f7c2
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-14 23:20:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
-    //Write your code here
+    event.preventDefault();  
 
+	var form = documtent.getElementByID('form1');
+
+	var firstName = form.elements['fname'].value; 
+	var lastName = form.elements['lname'].value; 
+	alert(firstname + " " + lastName);
 }
+document.getElementById('form1').onsubmit = getFormvalue;
