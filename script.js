db4572510f6efdba870b29f2d3894edc90979eb6
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-02-14 23:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function getFormvalue() {
-    event.preventDefault();  
+function getFormvalue(event) {
+  // Prevent the default form submission
+  event.preventDefault();
 
-	var form = documtent.getElementByID('form1');
+  // Get the form
+  var form = document.getElementById('form1');
 
-	var firstName = form.elements['fname'].value; 
-	var lastName = form.elements['lname'].value; 
-	alert(firstname + " " + lastName);
+  // Get the values of the first name and last name
+  var firstName = form.elements['fname'].value;
+  var lastName = form.elements['lname'].value;
+
+  // Print the first and last name of the user using alert
+  alert(firstName + ' ' + lastName);
 }
+
+// Add event listener to the form on submit
 document.getElementById('form1').onsubmit = getFormvalue;
